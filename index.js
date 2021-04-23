175427c7fe2fef055e10ee2e59dcf232f616d73d
pfebri32 <personal.febri32@gmail.com>
2021-04-24 01:16:16 +0700

    Setting up backend

---
+++ index.js
@@ -0,0 +1,12 @@
+// Imports.
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Your server is start on port ${process.env.PORT}...`);
+});
