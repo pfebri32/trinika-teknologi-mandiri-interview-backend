e244025422826cbdb94296bb6648561e4c594269
pfebri32 <personal.febri32@gmail.com>
2021-04-24 01:28:23 +0700

    Init sequelize

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Imports.
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
---
+++ src/routes/v1/index.js
@@ -0,0 +1,8 @@
+// Imports.
+const router = require('express').Router();
+
+// ***********************
+// ***** ROUTERS MAP *****
+// ***********************
+
+module.exports = router;
