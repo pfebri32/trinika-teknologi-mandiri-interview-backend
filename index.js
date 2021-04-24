1a24d3e7b6b1877d537a9ee19c9a06a29ebc19f2
pfebri32 <personal.febri32@gmail.com>
2021-04-24 10:42:10 +0700

    Requirement API and refresh

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/v1', routeV1);
+app.use('/uploads/', express.static('uploads'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Your server is start on port ${process.env.PORT}...`);
--- src/controllers/v1/event.js
+++ src/controllers/v1/event.js
@@ -5,16 +5,18 @@ const { Event, PersonInEvent, Person } = require('../../../models');
 // Req. upload middleware.
 // Form data characteristic.
 exports.addEvent = async (req, res) => {
-  const { body } = req;
+  const { body, files } = req;
+  const participants = JSON.parse(body.participants);
 
   // Validation.
   const schema = Joi.object({
     title: Joi.string().required(),
     location: Joi.string().required(),
-    note: Joi.string().min(51).required(),
+    note: Joi.string().min(50).required(),
     date: Joi.date().required(),
+    participants: Joi.array().items(Joi.string()).min(1),
   });
-  const { error } = schema.validate(body);
+  const { error } = schema.validate({ ...body, participants });
 
   if (error) {
     return res.status(422).send({
@@ -24,7 +26,22 @@ exports.addEvent = async (req, res) => {
   }
   try {
     // Create new event.
-    const event = await Event.create(body);
+    const event = await Event.create({
+      ...body,
+      image: files.image[0].filename,
+    });
+
+    // Create unique person.
+    for (let i = 0; i < participants.length; i++) {
+      let person = await Person.findOne({ where: { name: participants[i] } });
+      if (!person) {
+        person = await Person.create({ name: participants[i] });
+      }
+      await PersonInEvent.create({
+        eventId: event.id,
+        personId: person.id,
+      });
+    }
 
     res.send({
       status: 'success',
@@ -52,6 +69,12 @@ exports.getEvents = async (req, res) => {
         },
       },
     });
+
+    // Refactor.
+    for (let i = 0; i < events.length; i++) {
+      events[i].dataValues['image'] =
+        process.env.UPLOADS_URL + events[i].dataValues['image'];
+    }
 
     res.send({
       status: 'success',
